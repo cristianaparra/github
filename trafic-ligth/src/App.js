94a889dbe2b189b86a2a480264878ceef911f6d2
Cristian Parra <cristian.a.parra@gmail.com>
2020-08-15 00:28:02 -0400

    Trafic-ligth terminado

--- trafic-ligth/src/App.js
+++ trafic-ligth/src/App.js
@@ -1,39 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 
 
 const App = () => {
 
-
-
-
-    const [state, setState] = useState("", )
-    const [ligth, setLigth] = useState("",)
-    const [luz, setLuz] = useState("",)
-
-    useEffect(() => {
-        document.className = "";
-        return () => {
-            document.className = "red ligth ";
-        };
-      }, [state]);
+    const [state, setState] = useState("");
 
     return (
         <>
             <div id="traficcTop"></div>
             <div id="container">
 
-                <div className={"red ligth " + state} onClick={() => setState("selected")}></div>
-                <div className={"yellow ligth " + ligth} onClick={() => setLigth("selected")}></div>
-                <div className={"green ligth " + luz} onClick={() => setLuz("selected")}></div>
-
+                <div className={"red ligth " + (state == 'red' ? 'selected' : '')} onClick={() => setState("red")}></div>
+                <div className={"yellow ligth " + (state == 'yellow' ? 'selected' : '')} onClick={() => setState("yellow")}></div>
+                <div className={"green ligth " + (state == 'green' ? 'selected' : '')} onClick={() => setState("green")}></div>
 
             </div>
 
         </>
     )
-
-}
+};
 
 export default App;
